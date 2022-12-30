dd526db64a244e9c3d35b8f60dac9f84beaea89f
devJung55 <devJung55@gmail.com>
2022-12-30 22:00:00 +0900

    day02 study

---
+++ workspace/operator/src/controllStatement/WhileTest.java
@@ -0,0 +1,14 @@
+package controllStatement;
+
+public class WhileTest {
+
+	public static void main(String[] args) {
+		int count = 0;
+		
+		while(count < 10) {
+			count++;
+			System.out.println(count + ". Á¤Áö¿µ");
+		}
+	}
+
+}
